6aff57a90e05aa025803fffdd4ed670fdb269e7a
bekthat <begzat.kamen@gmail.com>
2026-03-08 18:47:04 +0500

    Add GET and POST methods

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Knetic/govaluate"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Calculation struct {
+	ID         string `json:"id"`
+	Expression string `json:"expression"`
+	Result     string `json:"result"`
+}
+
+type CalculationResult struct {
+	Expression string `json:"expression"`
+}
+
+var calculations = []Calculation{}
+
+func calculateExpression(expression string) (string, error) {
+	expr, err := govaluate.NewEvaluableExpression(expression)
+	if err != nil {
+		return "", err
+	}
+
+	result, err := expr.Evaluate(nil)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%v", result), nil
+}
+
+func getCalculations(c echo.Context) error {
+	return c.JSON(http.StatusOK, calculations)
+}
+
+func postCalculations(c echo.Context) error {
+	var req CalculationResult
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+	result, err := calculateExpression(req.Expression)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+	calc := Calculation{
+		ID:         uuid.NewString(),
+		Expression: req.Expression,
+		Result:     result,
+	}
+	calculations = append(calculations, calc)
+	return c.JSON(http.StatusCreated, calc)
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.GET("/calculations", getCalculations)
+	e.POST("/calculations", postCalculations)
+	e.Start("localhost:8080")
+}
